addc6452513fce51162330ac086a09eca806a5dc
장준호 <jjh2171@hybus.net>
2020-06-12 16:50:26 +0900

    add Submit func in login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,42 @@
 <template>
-    <div>
-    <form>
-    <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
-        <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
-    </div>
-    <div class="form-group">
-        <label htmlFor="password">Password</label>
-        <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
-        <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
-        <router-link to="/register" class="btn btn-link">Register</router-link>
-    </div>
+<div>
+    <form @submit.prevent="onSubmit(username, password)">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
+            <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
+        </div>
+        <div class="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
+            <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
+        </div>
+        <div class="form-group">
+            <button class="btn">Login</button>
+            <router-link to="/register" class="btn btn-link">Register</router-link>
+        </div>
     </form>
-    </div>
+</div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      passowrd: '',
+      msg: '',
+      submitted: false
+    }
+  },
+  methods: {
+    onSubmit (username, passowrd) {
+      this.submitted = true
+      if (username == null || passowrd == null) {
+        alert('ID 또는 Password를 입력해주세요.')
+        return
+      }
+      alert('id : ' + username + ' ' + 'pwd : ' + passowrd)
+    }
+  }
+}
+</script>
